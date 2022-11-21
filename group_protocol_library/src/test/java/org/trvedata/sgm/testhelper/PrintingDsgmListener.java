1535d01d24cc9421b91c571e4d3767289d17a2fe
alainga <55834361+alainga@users.noreply.github.com>
2022-11-21 18:23:40 +0100

    Update PrintingDsgmListener.java
    fix typo

--- group_protocol_library/src/test/java/org/trvedata/sgm/testhelper/PrintingDsgmListener.java
+++ group_protocol_library/src/test/java/org/trvedata/sgm/testhelper/PrintingDsgmListener.java
@@ -22,20 +22,20 @@ public class PrintingDsgmListener implements DsgmClient.DsgmListener {
 
     @Override
     public void onIncomingMessage(IdentityKey sender, byte[] plaintext) {
-        Logger.i("PrintingDczkaListener", myName + ": Incoming message from " + names.get(sender) +
+        Logger.i("PrintingDcgkaListener", myName + ": Incoming message from " + names.get(sender) +
                 ", plaintext: " + new String(plaintext));
     }
 
     @Override
     public void onUpdate(IdentityKey updater, Object messageId) {
         String messageIdString = (messageId instanceof MessageId) ? (", number: " + ((MessageId) messageId).number) : "";
-        Logger.i("PrintingDczkaListener", myName + ": Update from " + names.get(updater) + messageIdString);
+        Logger.i("PrintingDcgkaListener", myName + ": Update from " + names.get(updater) + messageIdString);
     }
 
     @Override
     public void onAdd(IdentityKey adder, IdentityKey added, Object messageId) {
         String messageIdString = (messageId instanceof MessageId) ? (", number: " + ((MessageId) messageId).number) : "";
-        Logger.i("PrintingDczkaListener", myName + ": Add from " + names.get(adder) +
+        Logger.i("PrintingDcgkaListener", myName + ": Add from " + names.get(adder) +
                 ", added: " + names.get(added) + messageIdString);
     }
 
@@ -45,7 +45,7 @@ public class PrintingDsgmListener implements DsgmClient.DsgmListener {
         String removedString = "[";
         for (IdentityKey oneRemoved : removed) removedString += names.get(oneRemoved) + ", ";
         removedString += "]";
-        Logger.i("PrintingDczkaListener", myName + ": Remove from " + names.get(remover) +
+        Logger.i("PrintingDcgkaListener", myName + ": Remove from " + names.get(remover) +
                 ", removed: " + removedString + messageIdString);
     }
 
@@ -56,6 +56,6 @@ public class PrintingDsgmListener implements DsgmClient.DsgmListener {
             MessageId ackedId = (MessageId) acked;
             messageIdString = ", acked message: (" + names.get(ackedId.author) + ", " + ackedId.number + ")";
         }
-        Logger.i("PrintingDczkaListener", myName + ": Ack from " + names.get(acker) + messageIdString);
+        Logger.i("PrintingDcgkaListener", myName + ": Ack from " + names.get(acker) + messageIdString);
     }
 }
