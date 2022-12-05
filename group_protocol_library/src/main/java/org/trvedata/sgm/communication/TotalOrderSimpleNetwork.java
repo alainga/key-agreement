1da1bc1a5cd41058089b69967432cd988053ac2f
alainga <55834361+alainga@users.noreply.github.com>
2022-12-05 18:06:52 +0100

    Add generated Message classes to comm pkg and malicious stuff to TotalOrderSimpleNetwork

--- group_protocol_library/src/main/java/org/trvedata/sgm/communication/TotalOrderSimpleNetwork.java
+++ group_protocol_library/src/main/java/org/trvedata/sgm/communication/TotalOrderSimpleNetwork.java
@@ -4,6 +4,9 @@ import org.apache.commons.lang3.tuple.Triple;
 
 import java.util.ArrayDeque;
 
+import org.trvedata.sgm.misc.Utils;
+import org.apache.thrift.TException;
+
 public class TotalOrderSimpleNetwork extends SimpleNetwork {
     private boolean isActive = false;
     private ArrayDeque<Triple<Client, Client, byte[]>> queuedMessages = new ArrayDeque<>(); // (sender, recipient, message)
@@ -15,7 +18,28 @@ public class TotalOrderSimpleNetwork extends SimpleNetwork {
     @Override
     public void broadcast(final Client sender, final byte[] message) {
         for (Client client : mIdentifierToClient.values()) {
-            if (client != sender) queuedMessages.add(Triple.of(sender, client, message));
+
+            //ALG: for testing accountability
+            if(sender.name == "mallet" && client.name == "poorBob"){
+                try {
+                    System.out.println("message = "+message); //todo find place of hash in it to modify
+                    AccountableDcgkaMessage maliciousMessage = new AccountableDcgkaMessage();
+                    Utils.deserialize(maliciousMessage, message);
+                    if(maliciousMessage.getType().getValue() == 1){ //do it only if its an update
+                        maliciousMessage.setHash(new byte[16]);
+                        byte[] mm = Utils.serialize(maliciousMessage);
+                        queuedMessages.add(Triple.of(sender, client, mm));
+                    } else {
+                        queuedMessages.add(Triple.of(sender, client, message));
+                    }
+                    
+                } catch (TException exc) {
+                    throw new IllegalArgumentException("Failed to deserialize in process", exc);
+                }
+            } else{
+                if (client != sender) queuedMessages.add(Triple.of(sender, client, message));
+
+            }
         }
         if (!isActive) {
             // Prevent recursive calls from reaching this block
